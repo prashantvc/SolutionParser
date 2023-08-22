b3ac31b1616c707cf990b6d08bed5e026fef5d4d
Prashant Cholachagudda <pvc@outlook.com>
2023-08-22 12:16:53 +0530

    Write project info to json file

--- Commands/SolutionParserCommand.cs
+++ Commands/SolutionParserCommand.cs
@@ -1,6 +1,10 @@
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.Build.Construction;
+using System.Collections.Concurrent;
+using Microsoft.Build.Definition;
+using MSProject = Microsoft.Build.Evaluation.Project;
+using System.Text.Json;
 
 namespace Commands;
 public sealed class SolutionParserCommand : Command<SolutionParserCommand.Settings>
@@ -22,21 +26,81 @@ public sealed class SolutionParserCommand : Command<SolutionParserCommand.Settin
 
         var sln = SolutionFile.Parse(settings.Solution);
 
-        var solution = new Solution
-        {
-            Name = Path.GetFileNameWithoutExtension(settings.Solution),
-            Path = settings.Solution
-        };
-
-
         var projs = sln.ProjectsInOrder.Where(prj => prj.ProjectType == SolutionProjectType.KnownToBeMSBuildFormat)
-            .Select(prj => new Project { Name = prj.ProjectName, Path = prj.AbsolutePath })
             .ToList();
 
-        solution.Projects = projs;
+        projs.ForEach(Console.WriteLine);
 
-        Console.WriteLine(solution);
+        var projects = new ConcurrentBag<Project>();
+        Parallel.ForEach(projs, proj =>
+        {
+            var p = GetProjectDetails(proj.ProjectName, proj.AbsolutePath);
+            Console.WriteLine(p);
+            projects.Add(p);
+        });
+
+        var allProjects = projects.ToList();
+
+        List<ProjectFile> designerFiles = new();
+
+        foreach (var proj in allProjects)
+        {
+            proj.CoreProject?.GetItems("AvaloniaXaml").ToList().ForEach(item =>
+            {
+                var filePath = Path.GetFullPath(item.EvaluatedInclude, proj.DirectoryPath ?? "");
+                var designerFile = new ProjectFile
+                {
+                    Path = filePath,
+                    TargetPath = proj.TargetPath,
+                    ProjectPath = proj.Path
+                };
+                designerFiles.Add(designerFile);
+            });
+        }
+
+        var json = new { settings.Solution, Projects = allProjects, Files = designerFiles };
+
+        var jsonStr = JsonSerializer.Serialize(json, new JsonSerializerOptions
+        {
+            WriteIndented = true,
+        });
+
+        string jsonFilePath = Path.Combine(Path.GetTempPath(), $"{Path.GetFileName(settings.Solution)}.json");
+        File.WriteAllTextAsync(jsonFilePath, jsonStr);
 
         return 0;
+    }
+
+    Project GetProjectDetails(string name, string projPath)
+    {
+        var proj = MSProject.FromFile(projPath, new ProjectOptions());
+
+        var assembly = proj.GetPropertyValue("TargetPath");
+        var outputType = proj.GetPropertyValue("outputType");
+        var desingerHostPath = proj.GetPropertyValue("AvaloniaPreviewerNetCoreToolPath");
+
+        var targetfx = proj.GetPropertyValue("TargetFramework");
+        var projectDepsFilePath = proj.GetPropertyValue("ProjectDepsFilePath");
+        var projectRuntimeConfigFilePath = proj.GetPropertyValue("ProjectRuntimeConfigFilePath");
+
+        var references = proj.GetItems("ProjectReference");
+        var referencesPath = references.Select(p => Path.GetFullPath(p.EvaluatedInclude, projPath)).ToArray();
+
+        return new Project
+        {
+            Name = name,
+            Path = projPath,
+            TargetPath = assembly,
+            OutputType = outputType,
+            DesignerHostPath = Path.GetFullPath(desingerHostPath),
+
+            TargetFramework = targetfx,
+            DepsFilePath = projectDepsFilePath,
+            RuntimeConfigFilePath = projectRuntimeConfigFilePath,
+
+            CoreProject = proj,
+            ProjectReferences = referencesPath
+
+        };
     }
 }
--- Models/Project.cs
+++ Models/Project.cs
@@ -1,7 +1,33 @@
+using System.Text.Json.Serialization;
+using MSProject = Microsoft.Build.Evaluation.Project;
+
 namespace Models;
 internal class Project
 {
     public required string Name { get; set; }
     public required string Path { get; set; }
-    public override string ToString() => $"{Name} ({Path})";
+
+    public required string TargetPath { get; set; }
+    public required string OutputType { get; set; }
+    public required string DesignerHostPath { get; set; }
+    public required string TargetFramework { get; set; }
+    public required string DepsFilePath { get; set; }
+    public required string RuntimeConfigFilePath { get; set; }
+    public required string[] ProjectReferences { get; set; }
+
+    [JsonIgnore]
+    public MSProject? CoreProject { get; set; } = null;
+
+    public string? DirectoryPath => System.IO.Path.GetDirectoryName(Path);
+    public override string ToString() => $"{Name} ({OutputType}) - {DesignerHostPath}";
+}
+
+internal class ProjectFile
+{
+    public required string Path { get; set; }
+
+    public required string TargetPath { get; set; }
+    public required string ProjectPath { get; set; }
+
+    public override string ToString() => $"{Path} ({TargetPath}) - {ProjectPath}";
 }
